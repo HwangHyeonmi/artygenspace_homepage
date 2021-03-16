ca270003cb53a63b86f83e38946091a12f9eed9d
hwanghyeonmi <brownrice0916@naver.com>
2021-03-16 18:35:18 +0900

    artygen_webpage

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>작업중입니다.^^</p>
+        <a
+          className="App-link"
+          /* href="https://reactjs.org" */
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ★Artygen Space★
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
