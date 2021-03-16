b92701d569fc1af9953f4c799faabb230868bee0
hwanghyeonmi <brownrice0916@naver.com>
2021-03-16 22:45:26 +0900

    일단 저장

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>작업중입니다.^^</p>
         <a
           className="App-link"
-          /* href="https://reactjs.org" */
+          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           ★Artygen Space★
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
